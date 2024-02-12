2b67b7d7bbe1fc6d74a0cf3f321b473411f90787
Toritho17 <133843547+toritho17@users.noreply.github.com>
2024-02-12 16:09:39 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itesca/practica6/MenuActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itesca.practica6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
---
+++ app/src/main/java/mx/edu/itesca/practica6/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package mx.edu.itesca.practica6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val button: Button = findViewById(R.id.sign_in_button)
+
+        button.setOnClickListener{
+            var intent = Intent(this,MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    }
